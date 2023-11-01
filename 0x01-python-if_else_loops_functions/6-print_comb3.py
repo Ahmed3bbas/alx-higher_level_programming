4036c02de8afc2a462c26b7a8593a760693a1c10
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 09:55:56 +0200

    try2: 6-print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,10 @@
 
 comb = ""
 for i in range(10):
-    for j in range(10):
-        if comb.find(str(j) + str(i)) == -1:
-            comb += (str(i) + str(j))
-            comb += ", "
+    for j in range(i + 1, 10):
+        # if comb.find(str(j) + str(i)) == -1:
+        comb += (str(i) + str(j))
+        comb += ", "
 
 # remove last space and comma
 comb = comb[:-2]
