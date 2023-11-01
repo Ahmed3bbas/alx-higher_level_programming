1429988a63304d4a554e2ea377e13662888c0f6e
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 07:09:37 +0200

    try 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-alpthbet = ""
-for i in "abcdefghijklmnopqrstuvwxyz":  # for i in range(97, 123) ==> chr(i)
-    alpthbet += i
-print("{}".format(alpthbet), end="")
+# alpthbet = ""
+# for i in "abcdefghijklmnopqrstuvwxyz":  # for i in range(97, 123) ==> chr(i)
+#     alpthbet += i
+print("{}".format("abcdefghijklmnopqrstuvwxyz"), end="")
