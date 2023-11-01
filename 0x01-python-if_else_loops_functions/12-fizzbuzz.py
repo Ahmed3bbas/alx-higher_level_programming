d57bc0ee7c68a35974a421b97d5dd32c4326e491
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 09:31:44 +0200

    add: 12-fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    pattern = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            pattern += "FizzBuzz"
+        elif i % 3 == 0:
+            pattern += "Fizz"
+        elif i % 5 == 0:
+            pattern += "Buzz"
+        else:
+            pattern += str(i)
+        pattern += " "
+    pattern = pattern[:-1]  # remove last comma and space
+    print("{}".format(pattern))
