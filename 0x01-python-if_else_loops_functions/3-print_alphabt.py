9b0da86640f41ca02fd8be6edae2ea1266c0f4a1
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 07:26:30 +0200

    add: 3-print_alphabet

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+alpha = ""
+for i in range(97, 123):
+    # print(i, chr(i))
+    if i != 113 and i != 101:  # DOn'r print e and q
+        alpha += chr(i)
+print("{}".format(alpha))
