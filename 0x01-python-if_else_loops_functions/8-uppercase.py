5daadb67397898498db3db987bc54d60e818502e
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 09:04:28 +0200

    add: 8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    newstr = ""
+    for c in str:
+        if ord(c) in range(97, 123):  # is lowercase char
+            newstr += chr(ord(c) - 32)
+        else:  # ord(c) in range(65, 91)
+            newstr += c
+    print(newstr)
