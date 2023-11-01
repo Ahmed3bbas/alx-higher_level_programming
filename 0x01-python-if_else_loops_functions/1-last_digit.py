172e0cec508c74799f198969d0c1d67e7522a514
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-01 04:49:19 +0200

    fix to pop8 style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 
 if number < 0:
     LD = ((number) - (number // 10) * 10)
-    LD = LD -  10
+    LD = LD - 10
 else:
-    LD = ((number) - (number // 10) * 10) 
+    LD = ((number) - (number // 10) * 10)
 
 if (LD > 5):
     print(f"Last digit of {number} is {LD} and is greater than 5")
 elif (LD == 0):
-    print(f"Last digit of {number} is {LD} is 0")
+    print(f"Last digit of {number} is {LD} and is 0")
 else:
     print(f"Last digit of {number} is {LD} and is less than 6 and not 0")
