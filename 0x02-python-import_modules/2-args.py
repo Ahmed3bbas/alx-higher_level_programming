61cf3e6180d377e4019b89f1d7247dbd147f74ac
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-11-03 21:38:16 +0200

    try: 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len == 0:
         print("0 arguments.")
     elif len == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(len))
 
